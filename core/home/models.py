9ff8d735d4951978dfb6448a8efefb843aa8d95a
golu7059 <golukumarjaisidih@gmail.com>
2024-03-29 00:03:04 +0530

    studied about model and migration

--- core/home/models.py
+++ core/home/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class student(models.Model):
+    # id = models.AutoField() this is by default add my django and it's primary key
+    name = models.CharField(max_length = 100)
+    age = models.IntegerField()
+    email = models.EmailField()
+    address = models.TextField()
+
+
+
